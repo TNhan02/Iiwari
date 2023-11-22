97b3958ba48f17115bdadd9a24cebad7bbff1027
0pepperi0 <komlevv652@gmail.com>
2023-11-22 10:24:42 +0200

    Add a vector library to simplify direction calculations

---
+++ botVector.py
@@ -0,0 +1,53 @@
+import math
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    
+    def getX(self):
+        return self.x
+    
+    def getY(self):
+        return self.y
+    
+
+
+class Vector():
+    def __init__(self, finalPoint: Point, initialPoint: Point):
+        self.x = finalPoint.getX() - initialPoint.getX()
+        self.y = finalPoint.getY() - initialPoint.getY()
+
+
+    def getX(self):
+        return self.x
+
+
+    def getY(self):
+        return self.y
+    
+
+    def countMagnitude(self):
+        return math.sqrt(self.x**2 + self.y**2)
+    
+
+def scalarProduct(vector1: Vector, vector2: Vector) -> float:
+    return (vector1.getX() * vector2.getX()) + (vector1.getY() * vector2.getY())
+
+
+def moduleProduct(vector1: Vector, vector2: Vector) -> float:
+    return vector1.countMagnitude() * vector2.countMagnitude()
+
+
+def angleBetweenVectors(vector1: Vector, vector2: Vector):
+
+    """
+    Returns a magnitude of an angle between 2 vectors.
+    The result is in radians from 0 to pi.
+    """
+
+    cos_a = scalarProduct(vector1, vector2) / moduleProduct(vector1, vector2)
+    return math.acos(cos_a*180/math.pi)
+
+    
--- robot.py
+++ robot.py
@@ -1,4 +1,5 @@
 #this is the 2nd python script to add robot's algorithm
+from botVector import *
 from enum import Enum
 import location_tracking_2023 as data_streaming
 from .Freenove_4WD_Smart_Car_Kit_for_Raspberry_Pi.Code.Server.Motor import *
@@ -20,11 +21,38 @@ motor = Motor()
 light = Light()
 status = RobotStatus()
 
-def calculate_delta_x(person_x, robot_x):
-    return person_x - robot_x
+# declare main points' coordinates
 
-def calculate_delta_y(person_y, robot_y):
-    return person_y, robot_y
+person_x = 0
+person_y = 0
+bot_x = 0
+bot_y = 0
+
+#should return a current position of a tag
+def getLocation(tag_code: str) -> Point:
+    pass
+
+
+def move():
+    motor(1000, 1000, 1000, 1000)
+
+# for demonstration purpose 
+# the main idea for the demonstration algorithm is to align first (x-coordinate) 
+# then (y-coordinate) with usage of vectors
+def movement(p_location: Point, buttonState: bool, state: int) -> None:
+    r_location = getLocation(robot_tag)
+    (r_x, r_y) = (r_location.getX(), r_location.getY())
+    (p_x, p_y) = (p_location.getX(), )
+    
+    if(r_x - p_x > ):
+        motor.setMotorModel(-1000,-1000,-1000,-1000)
+    if(r_x < p_x):
+    
+
+
+    
+
+
 
 def approaching_person(tag_id):
     data = data_streaming.button_pressed(site, token)
