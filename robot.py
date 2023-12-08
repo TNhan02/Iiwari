779c2242ed8e7b6abdbfd543788069688d96ed2a
0pepperi0 <komlevv652@gmail.com>
2023-12-08 12:10:13 +0200

    chahged

--- robot.py
+++ robot.py
@@ -1,12 +1,12 @@
 #this is the 2nd python script to add robot's algorithm
 import time
-import settings
+from settings import person_location, is_button_pressed, robot_location
 import run_streams
 from botVector import *
 from person_stream import *
 from Freenove_4WD_Smart_Car_Kit_for_Raspberry_Pi.Code.Server.Motor import *
 from Freenove_4WD_Smart_Car_Kit_for_Raspberry_Pi.Code.Server.Light import *
-#from Iiwari.Freenove_4WD_Smart_Car_Kit_for_Raspberry_Pi.Code.Server.Light import *
+from Iiwari.Freenove_4WD_Smart_Car_Kit_for_Raspberry_Pi.Code.Server.Light import *
 from enum import Enum
 
 # this class is used for setting the robot's status when an event occurs
@@ -102,12 +102,12 @@ def turn(p_location: Point, r_location: Point) -> None:
 
 
 def check_approach(accept_distance: float) -> bool:
-    #change function of robot
-    r_location = settings.robot_location
+    #change function
+    r_location = getLocation()
     r_x = r_location.getX()
     r_y = r_location.getY()
 
-    #change function of person
+    #change function
     p_location = settings.person_location
     p_x = p_location.getX()
     p_y = p_location.getY()
@@ -119,31 +119,20 @@ def check_approach(accept_distance: float) -> bool:
         return True
     
 
-<<<<<<< HEAD
-def rotate(angle: float, person_y: float, robot_y: float) -> None:
+def rotate(angle: float) -> None:
     rotate_angle = 180-angle
     rotation_time = rotate_angle* (0.88/90)
-
-=======
-def rotate(angle: float, robot_y: float, person_y: float) -> None:
-    rotate_angle = 180-angle
-    rotation_time = rotate_angle* (0.88/90)
->>>>>>> 9edf045e23fe8f636dca62b2012007c990d72605
-    if(robot_y < person_y):
-        motor.setMotorModel(2000,2000,-2000,-2000)
-    elif(robot_y > person_y):
-        motor.setMotorModel(-2000,-2000,2000,2000)
+    motor.setMotorModel(2000,2000,-2000,-2000)
     time.sleep(rotation_time)
-    motor.setMotorModel(0,0,0,0)
+    motor.destroy()
 
 #Final version of the movement algorithm
 
 def movement() -> None:
-    #status = RobotStatus.IDLE.value
-    """
+    status = RobotStatus.IDLE
     while True:
         match (status):
-            case RobotStatus.IDLE.value:
+            case RobotStatus.IDLE:
                 if(settings.is_button_pressed):
                     status = RobotStatus.DUTY.value
                     break
@@ -151,15 +140,15 @@ def movement() -> None:
                     time.sleep(2)
                 continue
             
-            case RobotStatus.DUTY.value:
+            case RobotStatus.DUTY:
                 #change to appropriate function
-                r_locationInitial = settings.robot_location
+                r_locationInitial = getLocation(robot_tag)
                 motor.setMotorModel(1000,1000,1000,1000)
                 time.sleep(1)
                 motor.destroy()
                 #change to appropriate function
-                p_location = settings.person_location
-                r_locationCurrent = settings.robot_location
+                p_location = person_location
+                r_locationCurrent = robot_location
 
                 r_vector = Vector(r_locationCurrent, r_locationInitial)
                 r_p_vector = Vector(p_location, r_locationCurrent)
@@ -169,45 +158,18 @@ def movement() -> None:
 
                 while(not check_approach(5)):
                     motor.setMotorModel(1000,1000,1000,1000)
-                    time.sleep(0.3)
                 motor.destroy()
                 status = RobotStatus.IDLE.value
                 break
-    """
-    time.sleep(3)
-    motor.setMotorModel(1000,1000,1000,1000)
-    time.sleep(2)
-    motor.setMotorModel(0,0,0,0)
-    time.sleep(5)
-    r_locationInitial = settings.collector.getInitRobotLocation()
-    p_location = settings.collector.getPersonLocation()
-    motor.setMotorModel(-1000,-1000,-1000,-1000)
-    time.sleep(2)
-    motor.setMotorModel(0,0,0,0)
 
-    #robot location initially
-    print('Robot 1st location: [X:{}, Y:{}]'.format(r_locationInitial.getX(), r_locationInitial.getY()))
-    #person location initially
-    print('Person: [X:{}, Y:{}]'.format(p_location.getX(), p_location.getY()))
-    time.sleep(10)
-    r_locationCurrent = settings.collector.getRobotLocation()
-    print('Robot 2nd location: [X: {}, Y:{}]'.format(r_locationCurrent.getX(), r_locationCurrent.getY()))
-    
-    r_vector = Vector(r_locationCurrent, r_locationInitial)
-    r_p_vector = Vector(p_location, r_locationCurrent)
-    angle = angleBetweenVectors(r_vector, r_p_vector)
-    #calibration is required 
-    rotate(angle, r_locationCurrent.getY(), p_location.getY())
-    rotate(angle, r_locationCurrent.getY(), p_location.getY())
-
-    while(not check_approach(10)):
-        motor.setMotorModel(1000,1000,1000,1000)
-        time.sleep(0.3)
-    motor.setMotorModel(0,0,0,0)
 
 def main():
-    movement()
+    #test values
+    p_location = Point(0, person_y)
+    r_location = Point()
+
+    turn(p_location, r_location)
 
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     main()
